1dea58819250e21f0b201182db03e33b923384ec
nazapasa <nazaarismendi.99@gmail.com>
2022-11-26 20:24:56 -0500

    agregar footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
 import { FormularioContactoComponent } from './formulario-contacto/formulario-contacto.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormularioRegistroUsuarioComponent,
-    FormularioContactoComponent
+    FormularioContactoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
