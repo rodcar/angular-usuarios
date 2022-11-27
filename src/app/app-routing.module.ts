fefb197d53fb53ca7c6702b97d07e8873bab144d
rodcar <ivanrod12@hotmail.com>
2022-11-27 15:47:58 -0500

    agrega la página de cpntacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { FormularioContactoComponent } from './formulario-contacto/formulario-contacto.component';
 import { FormularioRegistroUsuarioComponent } from './formulario-registro-usuario/formulario-registro-usuario.component';
 import { HomeComponent } from './home/home.component';
+import { ContactoPageComponent } from './pages/contacto-page/contacto-page.component';
 import { TablaUsuariosComponent } from './tabla-usuarios/tabla-usuarios.component';
 
 const routes: Routes = [
@@ -16,7 +17,7 @@ const routes: Routes = [
   },
   {
     path: 'contacto',
-    component: FormularioContactoComponent
+    component: ContactoPageComponent
   },
   {
     path: 'usuarios',
---
+++ src/app/pages/contacto-page/contacto-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-page',
+  templateUrl: './contacto-page.component.html',
+  styleUrls: ['./contacto-page.component.css']
+})
+export class ContactoPageComponent {
+
+}
